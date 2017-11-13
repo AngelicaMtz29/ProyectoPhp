098d8a2b1ef908a6b32ebac5fd8a8444a81e4ba3
Eduardo <hseduardo98@gmail.com>
2017-11-12 20:24:18 -0600

    No se si sean necesarios estos 2 archivos

---
+++ abrir_conexion.php
@@ -0,0 +1,18 @@
+
+<?php
+header('Content-Type: text/html; charset=utf-8');
+function my_error_handler($errno, $errstr, $errfile, $errline) {
+
+}
+set_error_handler("my_error_handler", E_WARNING);
+
+$conexion = oci_connect("eduardo","o200eya610l","localhost/XE");
+
+if (!$conexion){
+  $m = oci_error();
+  echo "Conexión fallida " . $m["message"];
+}
+restore_error_handler();
+
+
+?>
