fb925c1850a4fdaf26c0531da731ce795faf3ee5
Eduardo <hseduardo98@gmail.com>
2017-11-13 01:10:50 -0600

    No muestra un registro, tengo 4 pero me muestra 3

--- ControlVacaciones.php
+++ ControlVacaciones.php
@@ -3,37 +3,38 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Deposito</title>
-		
+
 	</head>
 	<body id="top">
 		<p>
 			<?php
 				//establecer la conexión
-				$conexion=oci_connect("Confi","ConfiA29","localhost/XE");
-				//capturar la consulta
-				$sql = "select * from Empleado" ;
-				$sql2='Select * from Vacaciones';
+				//$conexion=oci_connect("Confi","ConfiA29","localhost/XE");
+				include("abrir_conexion.php");
+								//capturar la consulta
+				//$sql = "select * from Empleado" ;
+				$sql="Select * from Vacaciones";
 				//preparar la sentencia
-				$consulta = oci_parse($conexion, $sql) ;
-				$consulta2=oci_parse($conexion,$sql2);
+				//$consulta = oci_parse($conexion, $sql) ;
+				$consulta=oci_parse($conexion,$sql);
 				//Ejecutar la sentencia
+				//$exec=oci_execute($consulta);
 				$exec=oci_execute($consulta);
-				$exec=oci_execute($consulta2);
 				// verifico si hubo resultados al ajecutar la consulta
-				$resul2 = oci_fetch_array($consulta2) ;
 				$resul = oci_fetch_array($consulta) ;
-				
-				if ($resul == 0 || $resul2==0)
+				//$resul = oci_fetch_array($consulta) ;
+
+				if (/*$resul == 0 || */$resul==0)
 					echo "No hay empleados registrados"  ;
 				oci_close($conexion);
 			?>
 		</p>
-	
 
-   
+
+
 						<h2>Control de vacaciones</h2>
-						
-					
+
+
 						<form id="form1" name="form1" method="post" action="Busqueda.php">
 							<table width="1000" border="0">
 								<tr>
@@ -43,31 +44,35 @@
 									<td align="center">Editar</td>
 									<td align="center">Eliminar</td>
 								</tr>
+						</tr>
 								<?php
-								while ($registro2 = oci_fetch_array($consulta2)) {
+								while ($registro = oci_fetch_array($consulta)) {
 								?>
 									<tr>
-										<td align="center"><?php echo $registro2[0] ; ?> </td>
-										<td align="center"><?php echo $registro2[1] ; ?> </td>
-										<td align="center"><?php echo $registro2[2] ; ?></td>
+										<td align="center"><?php echo $registro[0] ; ?> </td>
+										<td align="center"><?php echo $registro[1] ; ?> </td>
+										<td align="center"><?php echo $registro[2] ; ?></td>
 
 										<td align="center">
-											<label><input name="editar" id="editar" type="radio" value="<?php echo $registro2[0] ; ?>" /></label>
+											<label><input name="editar" id="editar" type="radio" value="<?php echo $registro[0] ; ?>" /></label>
 										</td>
 										<td align="center">
-											<input  name="eliminar" type="checkbox" id="eliminar" value="<?php echo $registro2[0] ; ?>" />
+											<input  name="eliminar" type="checkbox" id="eliminar" value="<?php echo $registro[0] ; ?>" />
 										</td>
+
 									</tr>
-									
 								<?php
 								}
 								?>
-								<tr >
+
+								<tr>
+									<td></td>
+									<td></td>
 								<td colspan="5" align="center"><input name="Submit" type="submit" id="Submit" value="Ejecutar" /></td>
-								</tr>
-								
+							</tr>
+
 							</table>
-					
-		
+</form>
+
 	</body>
 </html>
