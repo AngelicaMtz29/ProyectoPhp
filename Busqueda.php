6116e7ad91bce74e4cc54d16918a9494422797a4
Eduardo <hseduardo98@gmail.com>
2017-11-13 01:14:49 -0600

    Creo que no se hizo nada

--- Busqueda.php
+++ Busqueda.php
@@ -8,20 +8,20 @@
 <body>
 	<p>
 	  <?php
-	
-		//Añadir métodos para 
-		
+
+		//Añadir métodos para
+
 			$boton = $_POST['editar'] ;
 
 			if ($boton == "Ejecutar") {
-				$conexion = oci_connect("Confi", "ConfiA29", "localhost/XE");		
+				$conexion = oci_connect("Confi", "ConfiA29", "localhost/XE");
 		// Para Actualizar...
 			// isset sirve para preguntar ¿si no está vacío?
 				if (isset ($_POST['editar'])) {
 				// se obtiene la información seleccionada del List/Menu
 				$fechainicio = $_POST['editar'] ;
 				//convertir lo que se ingresa a un tipo de datos date
-				
+
 				// se estructura la consulta
 				$no_empleado=$_POST['editar'];
 				$sql = "select * from Vacaciones where NoEmpleado=$no_empleado" ;
@@ -32,29 +32,29 @@
 				$exec=oci_execute($consulta);
 				// El resultado de la consulta se guarda en un arreglo
 				$registro = oci_fetch_array($consulta);
-				
-		
+
+
 				if(!$consulta)
-					echo "Error en la base de datos" ;		
+					echo "Error en la base de datos" ;
 				oci_close($conexion);
-			
+
 	?>
-	  
+
 				  <!-- Se crea un nuevo formulario en el que se colocará la información del producto seleccionado -->
 				  <!-- nótese a dónde lleva el action de este formulario -->
 			</p>
 				<p>Editar información   </p>
 				<form id="form1" name="form1" method="post" action="actualizarDatos.php">
 			  <label>No. Empleado:
-			  
+
 			  <!-- La información que tendrán los campos de este formulario, la obtenemos del arreglo en sus diferentes posiciones -->
-				
-			  <input name="no_empleado" type="text" id="no_empleado" value = "<?php echo $registro[0] ; ?>" /> 
+
+			  <input name="no_empleado" type="text" id="no_empleado" value = "<?php echo $registro[0] ; ?>"  disabled />
 			  </label>
-			 
+
 			  <p>
 			  <label>Fecha de inicio:
-			  <input name="fechainicio" type="text" id="fechainicio"  value="<?php echo $registro[1] ; ?>"/>
+			  <input name="fechainicio" type="text" id="fechainicio"  value="<?php echo $registro[1] ; ?>" />
 			  </label>
 			  </p>
 			  <p>
@@ -70,35 +70,35 @@
 			</form>
 				<?php
 							}
-					
+
 				?>
-	
+
 		<?php
 
 		// Para Eliminar...
 
 			if (isset ($_POST['eliminar'])) {
 				$fechainicio = $_POST['eliminar'] ;
-				
+
 				foreach ($fechainicio as $op) {
 					$sql = "delete from Vacaciones where FechaInicio = '$op'" ;
 					$consulta = oci_parse ($conexion, $sql);
 					//ejecutar la sentencia
 					$exec=oci_execute($consulta);
 				}
-						
-					oci_close($conexion);	
+
+					oci_close($conexion);
 					include('ControlVacaciones.php') ;
 			}
-			
-		}else 
+
+		}else
 			include('addVacaciones.php') ;
-			
-			
+
+
 
 
 		?>
 
-	
+
 </body>
 </html>
